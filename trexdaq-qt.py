d93800d27b52f9790703893c18fdd201f38eb3c0
Jonas Rembser <jonas.rembser@cern.ch>
2019-02-19 00:36:45 +0100

    we have learned a lot...

---
+++ trexdaq-qt.py
@@ -0,0 +1,160 @@
+import sys
+import time
+
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QAction, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QSpacerItem, QSizePolicy, QPushButton
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QSize, QThread, pyqtSignal
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+import random
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+
+import numpy as np
+
+# Basic skeleton adapted from:
+# https://stackoverflow.com/questions/48140576/matplotlib-toolbar-in-a-pyqt5-application
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        self.title = 'test'
+        self.left = 10
+        self.top = 10
+        self.width = 800
+        self.height = 600
+
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.statusBar().showMessage('Ready')
+
+        mainMenu = self.menuBar()
+        mainMenu.setNativeMenuBar(False)
+        fileMenu = mainMenu.addMenu('File')
+        helpMenu = mainMenu.addMenu('Help')
+
+        exitButton = QAction(QIcon('exit24.png'), 'Exit', self)
+        exitButton.setShortcut('Ctrl+Q')
+        exitButton.setStatusTip('Exit application')
+        exitButton.triggered.connect(self.close)
+        fileMenu.addAction(exitButton)
+
+
+        widget =  QWidget(self)
+        self.setCentralWidget(widget)
+        vlay = QVBoxLayout(widget)
+        hlay = QHBoxLayout()
+        vlay.addLayout(hlay)
+
+        self.nameLabel = QLabel('Name:', self)
+        self.line = QLineEdit(self)
+        self.nameLabel2 = QLabel('Result', self)
+
+        hlay.addWidget(self.nameLabel)
+        hlay.addWidget(self.line)
+        hlay.addWidget(self.nameLabel2)
+        hlay.addItem(QSpacerItem(1000, 10, QSizePolicy.Expanding))
+
+        pybutton = QPushButton('Click me', self)
+        pybutton.clicked.connect(self.clickMethod)
+        hlay2 = QHBoxLayout()
+        hlay2.addWidget(pybutton)
+        hlay2.addItem(QSpacerItem(1000, 10, QSizePolicy.Expanding))
+        vlay.addLayout(hlay2)
+        self.widgetPlot = WidgetPlot(self)
+        vlay.addWidget(self.widgetPlot)
+
+    def clickMethod(self):
+        print('Clicked Pyqt button.')
+
+        if self.line.text() == '':
+            self.statusBar().showMessage('Not a Number')
+        else:
+            print('Number: {}'.format(float(self.line.text())*2))
+            self.statusBar().showMessage('Introduction of a number')
+            self.nameLabel2.setText(str(float(self.line.text())*2))
+
+class WidgetPlot(QWidget):
+    def __init__(self, *args, **kwargs):
+        QWidget.__init__(self, *args, **kwargs)
+        self.setLayout(QVBoxLayout())
+        self.canvas = HistCanvas(self, width=10, height=8)
+        self.toolbar = NavigationToolbar(self.canvas, self)
+        self.layout().addWidget(self.toolbar)
+        self.layout().addWidget(self.canvas)
+
+    def foo(self, x):
+        self.canvas.figure.clf()
+        self.canvas.digitize(x)
+        self.canvas.plot()
+
+
+class HistCanvas(FigureCanvas):
+    def __init__(self, parent=None, width=10, height=8, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+        FigureCanvas.setSizePolicy(self, QSizePolicy.Expanding, QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+
+        n_bins = 2**8
+        self.bins = np.linspace(0, n_bins, n_bins+1)
+        self.content = np.zeros(n_bins+2, dtype=np.int)
+
+        self.plot()
+
+    def digitize(self, x):
+        for n in np.digitize(x, self.bins, right=True):
+            self.content[n] += 1
+
+    def plot(self):
+        data = [random.random() for i in range(250)]
+        ax = self.figure.add_subplot(111)
+        # ax.step(self.bins, np.concatenate([[0], self.content[1:-1]]))
+        ax.fill_between(self.bins, np.concatenate([[0], self.content[1:-1]]), step="pre")
+        # ax.set_title("Trexdaq Histogram")
+        ax.set_ylabel("counts")
+        ax.set_ylim(bottom=0.0)
+        ax.set_xlim(self.bins[0], self.bins[-1])
+        self.draw()
+
+# To get a background thread for acquiring the data, inspired from:
+# https://stackoverflow.com/questions/6783194/background-thread-with-qthread-in-pyqt
+class AquisitionThread(QThread):
+
+    sig = pyqtSignal(list)
+    refresh_rate = 1./60.
+
+    def run(self):
+        start_time = time.time()
+        data = []
+        for line in sys.stdin:
+            line = line.strip().split(" ")
+            debug_amplitudes = float(line[0])
+            measurements = [int(s) for s in line[1:]]
+            amplitude = max(measurements) - min(measurements)
+            print(amplitude)
+            data.append(amplitude)
+            if time.time() - start_time > self.refresh_rate:
+                print("Emitting!")
+                print(data)
+                self.sig.emit(data)
+                data = []
+                start_time = time.time()
+        self.sig.emit(data)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    mainWindow = MainWindow()
+    mainWindow.show()
+    thread = AquisitionThread()
+    thread.sig.connect(mainWindow.widgetPlot.foo)
+    # The thread finishing will not be connected to anything because it is not supposed to finish
+    # thread.finished.connect(app.exit)
+    thread.start()
+    sys.exit( app.exec_())
--- trexdaq.py
+++ trexdaq.py
@@ -1,160 +1,251 @@
+import pygame
 import sys
+from pygame.locals import *
 import time
-
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QAction, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QSpacerItem, QSizePolicy, QPushButton
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import QSize, QThread, pyqtSignal
-
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
-import random
-
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
-
 import numpy as np
+import h5py
 
-# Basic skeleton adapted from:
-# https://stackoverflow.com/questions/48140576/matplotlib-toolbar-in-a-pyqt5-application
+'''Test the BitScope Library by connecting with the first available device
+and performing a capture and dump. Requires BitLib 2.0 and Python Bindings'''
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        QMainWindow.__init__(self)
+import matplotlib.pyplot as plt
+import numpy as np
+import concurrent.futures
 
-        self.title = 'test'
-        self.left = 10
-        self.top = 10
-        self.width = 800
-        self.height = 600
+np.random.seed(10)
 
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
+rate = 200
 
-        self.statusBar().showMessage('Ready')
+# Define the pulse shape: a simple gaussian
+def pulse_shape(t, t0=5., a=12., sigma=1.):
+    return np.exp(-((t - t0)/(2.*sigma))**2) * a
 
-        mainMenu = self.menuBar()
-        mainMenu.setNativeMenuBar(False)
-        fileMenu = mainMenu.addMenu('File')
-        helpMenu = mainMenu.addMenu('Help')
+# Digitize to 8 bit plus saturation
+resolution = 2**8
+digitization_bins = np.linspace(0, 12, resolution+1)
+digitization_step = digitization_bins[1] - digitization_bins[0]
+def digitize(y):
+    y = np.digitize(y, digitization_bins)
+    y = np.clip(y, a_min=1, a_max=resolution)
+    return y - 1
 
-        exitButton = QAction(QIcon('exit24.png'), 'Exit', self)
-        exitButton.setShortcut('Ctrl+Q')
-        exitButton.setStatusTip('Exit application')
-        exitButton.triggered.connect(self.close)
-        fileMenu.addAction(exitButton)
+# set up pygame
+pygame.init()
+
+# set up the window
+window_width = 500
+window_height = 400
+window_surface = pygame.display.set_mode((window_width, window_height), 0, 32)
+pygame.display.set_caption('Trex DAQ')
+
+# set up the colors
+# WHITE = (220, 220, 204)
+text_color = (220, 220, 204)
+WHITE = (63, 63, 63)
+BLACK = (63, 63, 63)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+# BLUE = (0, 0, 255)
+# BLUE = (73, 114, 189)
+BLUE = (220, 220, 204)
+
+# set up fonts
+monospace_font = pygame.font.SysFont("DejaVu Sans Mono", 14)
 
 
-        widget =  QWidget(self)
-        self.setCentralWidget(widget)
-        vlay = QVBoxLayout(widget)
-        hlay = QHBoxLayout()
-        vlay.addLayout(hlay)
+# draw the white background onto the surface
+window_surface.fill(WHITE)
 
-        self.nameLabel = QLabel('Name:', self)
-        self.line = QLineEdit(self)
-        self.nameLabel2 = QLabel('Result', self)
+def draw_prompt(text=": Hello World!"):
 
-        hlay.addWidget(self.nameLabel)
-        hlay.addWidget(self.line)
-        hlay.addWidget(self.nameLabel2)
-        hlay.addItem(QSpacerItem(1000, 10, QSizePolicy.Expanding))
+    # set up the text
+    text = monospace_font.render(text, True, text_color, BLACK)
+    textRect = text.get_rect()
+    textRect.top = window_height - 18
+    textRect.left = 2
 
-        pybutton = QPushButton('Click me', self)
-        pybutton.clicked.connect(self.clickMethod)
-        hlay2 = QHBoxLayout()
-        hlay2.addWidget(pybutton)
-        hlay2.addItem(QSpacerItem(1000, 10, QSizePolicy.Expanding))
-        vlay.addLayout(hlay2)
-        self.widgetPlot = WidgetPlot(self)
-        vlay.addWidget(self.widgetPlot)
+    # draw the text's background rectangle onto the surface
+    pygame.draw.rect(window_surface, BLACK, (0, textRect.top - 2, window_width, textRect.height + 4))
+    # draw the text onto the surface
+    window_surface.blit(text, textRect)
 
-    def clickMethod(self):
-        print('Clicked Pyqt button.')
+# draw a green polygon onto the surface
+pygame.draw.polygon(window_surface, GREEN, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
 
-        if self.line.text() == '':
-            self.statusBar().showMessage('Not a Number')
+def draw_hist(bin_contents, logscale=False):
+    n = len(bin_contents)
+    max_content = max(max(bin_contents), 1)
+    max_content = 10**(int(np.log10(max_content))+1)
+
+    if logscale:
+        max_content = np.log(max_content)
+
+    w = 500
+    h = 400 - 20
+
+    y_scale = float(h)/max_content
+    x_scale = w/float(n+1)
+
+    points = [(0.,h)]
+    for i, c in enumerate(bin_contents):
+        if logscale:
+            c = max(0, np.log(c))
+        points.append((x_scale*i, h - y_scale*c))
+        points.append((x_scale*(i+1), h - y_scale*c))
+    points.append((w,h))
+
+    pygame.draw.polygon(window_surface, BLUE, points)
+
+# draw some blue lines onto the surface
+pygame.draw.line(window_surface, BLUE, (60, 60), (120, 60), 4)
+pygame.draw.line(window_surface, BLUE, (120, 60), (60, 120))
+pygame.draw.line(window_surface, BLUE, (60, 120), (120, 120), 4)
+
+# draw a blue circle onto the surface
+pygame.draw.circle(window_surface, BLUE, (300, 50), 20, 0)
+
+# draw a red ellipse onto the surface
+pygame.draw.ellipse(window_surface, RED, (300, 250, 40, 80), 1)
+
+
+# get a pixel array of the surface
+pixArray = pygame.PixelArray(window_surface)
+pixArray[480][380] = BLACK
+del pixArray
+
+# draw the window onto the screen
+pygame.display.update()
+
+# initialize amplitudes  storage
+amplitudes = []
+data = []
+
+is_running = False
+logscale = False
+show_prompt = True
+prompt_mode = False
+
+prompt_text = ""
+
+about_to_clear = False
+about_to_clear_time = 0
+
+# the value for saturation is 9.1640625
+# bins = np.linspace(0, 9.165, 2**11+1)
+bins = np.linspace(0, 2**8, 2**8+1)
+
+acquisitor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+
+def acquire():
+    while True:
+        if is_running:
+            line = input().strip()
+            line = line.split(" ")
+            line = [float(x) for x in line[1:]]
+            amp = np.max(line) - np.min(line)
+            amplitudes.append(amp)
+            data.append(line)
         else:
-            print('Number: {}'.format(float(self.line.text())*2))
-            self.statusBar().showMessage('Introduction of a number')
-            self.nameLabel2.setText(str(float(self.line.text())*2))
+            dump = input()
 
-class WidgetPlot(QWidget):
-    def __init__(self, *args, **kwargs):
-        QWidget.__init__(self, *args, **kwargs)
-        self.setLayout(QVBoxLayout())
-        self.canvas = HistCanvas(self, width=10, height=8)
-        self.toolbar = NavigationToolbar(self.canvas, self)
-        self.layout().addWidget(self.toolbar)
-        self.layout().addWidget(self.canvas)
+acquisitor.submit(acquire)
 
-    def foo(self, x):
-        self.canvas.figure.clf()
-        self.canvas.digitize(x)
-        self.canvas.plot()
+refresh_rate = 1./60
 
+# run the game loop
+while True:
 
-class HistCanvas(FigureCanvas):
-    def __init__(self, parent=None, width=10, height=8, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        FigureCanvas.__init__(self, fig)
-        self.setParent(parent)
-        FigureCanvas.setSizePolicy(self, QSizePolicy.Expanding, QSizePolicy.Expanding)
-        FigureCanvas.updateGeometry(self)
+    if about_to_clear and time.time() - about_to_clear_time >= 0.5:
+        about_to_clear = False
 
-        n_bins = 2**8
-        self.bins = np.linspace(0, n_bins, n_bins+1)
-        self.content = np.zeros(n_bins+2, dtype=np.int)
+    time.sleep(refresh_rate)
 
-        self.plot()
+    if is_running:
+        window_surface.fill(WHITE)
 
-    def digitize(self, x):
-        for n in np.digitize(x, self.bins, right=True):
-            self.content[n] += 1
+        draw_hist(np.histogram(amplitudes, bins)[0], logscale=logscale)
 
-    def plot(self):
-        data = [random.random() for i in range(250)]
-        ax = self.figure.add_subplot(111)
-        # ax.step(self.bins, np.concatenate([[0], self.content[1:-1]]))
-        ax.fill_between(self.bins, np.concatenate([[0], self.content[1:-1]]), step="pre")
-        # ax.set_title("Trexdaq Histogram")
-        ax.set_ylabel("counts")
-        ax.set_ylim(bottom=0.0)
-        ax.set_xlim(self.bins[0], self.bins[-1])
-        self.draw()
+    # draw the window onto the screen
+    if show_prompt:
+        draw_prompt(prompt_text)
+    pygame.display.update()
 
-# To get a background thread for acquiring the data, inspired from:
-# https://stackoverflow.com/questions/6783194/background-thread-with-qthread-in-pyqt
-class AquisitionThread(QThread):
+    for event in pygame.event.get():
 
-    sig = pyqtSignal(list)
-    refresh_rate = 1./60.
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
 
-    def run(self):
-        start_time = time.time()
-        data = []
-        for line in sys.stdin:
-            line = line.strip().split(" ")
-            debug_amplitudes = float(line[0])
-            measurements = [int(s) for s in line[1:]]
-            amplitude = max(measurements) - min(measurements)
-            print(amplitude)
-            data.append(amplitude)
-            if time.time() - start_time > self.refresh_rate:
-                print("Emitting!")
-                print(data)
-                self.sig.emit(data)
-                data = []
-                start_time = time.time()
-        self.sig.emit(data)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                prompt_text = ""
+                prompt_mode = False
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    mainWindow = MainWindow()
-    mainWindow.show()
-    thread = AquisitionThread()
-    thread.sig.connect(mainWindow.widgetPlot.foo)
-    # The thread finishing will not be connected to anything because it is not supposed to finish
-    # thread.finished.connect(app.exit)
-    thread.start()
-    sys.exit( app.exec_())
+                if show_prompt:
+                    draw_prompt(prompt_text)
+                pygame.display.update()
+
+            if prompt_mode:
+                if event.key == pygame.K_BACKSPACE:
+                    prompt_text = prompt_text[:-1]
+                elif event.key == pygame.K_ESCAPE:
+                    prompt_text = prompt_text[:-1]
+                elif event.key == pygame.K_RETURN:
+                    if prompt_text[:3] == ":w ":
+                        file_name = str(prompt_text[3:].strip())
+                        file_name = file_name.replace(".h5", "").replace(".hdf", "")
+                        np.savetxt(file_name, np.array(data), header="data")
+                        with h5py.File(file_name+".h5", 'w') as hf:
+                            hf.create_dataset("data",  data=np.array(data))
+                            hf.create_dataset("amplitudes",  data=np.array(amplitudes))
+                        prompt_text = "\""+file_name+"\".h5 written"
+                        prompt_mode = False
+                    elif prompt_text.strip() == ":set logscale":
+                        logscale = not logscale
+                        prompt_mode = False
+                    elif prompt_text.strip() == ":q":
+                        pygame.quit()
+                        sys.exit()
+                    else:
+                        prompt_text = "Not a command: " + prompt_text[1:]
+                        prompt_mode = False
+                else:
+                    prompt_text = prompt_text + event.unicode
+
+                if show_prompt:
+                    draw_prompt(prompt_text)
+                pygame.display.update()
+
+            else:
+                if event.key == pygame.K_l:
+                    logscale = not logscale
+                    window_surface.fill(WHITE)
+                    draw_hist(np.histogram(amplitudes, bins)[0],
+                              logscale=logscale)
+                    if show_prompt:
+                        draw_prompt(prompt_text)
+                    pygame.display.update()
+
+                if event.key == pygame.K_SPACE:
+                    is_running = not is_running
+
+                if event.key == 59: # colon
+                    if not prompt_mode:
+                        prompt_mode = True
+                        prompt_text = ":"
+                    if show_prompt:
+                        draw_prompt(prompt_text)
+                    pygame.display.update()
+
+                if event.key == pygame.K_d:
+                    if not is_running:
+                        if about_to_clear and time.time() - about_to_clear_time < 0.5:
+                            about_to_clear = False
+                            amplitudes = []
+                            data = []
+                            window_surface.fill(WHITE)
+                            if show_prompt:
+                                draw_prompt(prompt_text)
+                            pygame.display.update()
+                        about_to_clear = True
+                        about_to_clear_time = time.time()
